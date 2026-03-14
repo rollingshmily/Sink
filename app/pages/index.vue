3c76c150e0f879b99b276dbedf7f0eb45081edde
rollingshmily <62282098+rollingshmily@users.noreply.github.com>
2026-03-14 16:59:49 +0800

    Change home page to loading message during navigation
    Replaced the home page content with a loading message while navigating.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,8 @@
+<script setup lang="ts">
+// 把 '/dashboard' 换成你点仪表盘后实际跳过去的路径，比如有可能是 '/login'
+await navigateTo('/dashboard/login') 
+</script>
+
 <template>
-  <div class="flex flex-col justify-center">
-    <HomeHero />
-    <HomeLogos />
-    <HomeFeatures />
-    <HomeStats />
-    <HomeTestimonials />
-    <HomeCta />
-  </div>
+  <div>正在跳转到控制台...</div>
 </template>
