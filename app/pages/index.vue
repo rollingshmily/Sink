fbd67fe2545c0005788b6d6721a3870844216c4b
rollingshmily <62282098+rollingshmily@users.noreply.github.com>
2026-03-14 17:06:45 +0800

    Refactor navigation to use page meta redirect

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-// 把 '/dashboard' 换成你点仪表盘后实际跳过去的路径，比如有可能是 '/login'
-await navigateTo('/dashboard/login') 
+definePageMeta({
+  redirect: '/dashboard/login'
+})
 </script>
-
-<template>
-  <div>正在跳转到控制台...</div>
-</template>
